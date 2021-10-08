5007971c671ce134ef1e0521082b1ac2c61486a0
Dimitrije <test@gmail.com>
2021-10-08 19:59:38 +0200

    Datentypen intitialisieren

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,11 @@
 public class Calculator {
 
     public static void main(String[] args) {
-
+        short kleinsterDatentyp = 1;
+        int mittelDatentyp = 123;
+        long großerDatentyp = 13456987;
+        char a = 'a';
+        String Zeichenkette = "TEST";
+        System.out.println(kleinsterDatentyp + " " + mittelDatentyp + "  " + großerDatentyp + "  " + a + "  " + Zeichenkette);
     }
 }
