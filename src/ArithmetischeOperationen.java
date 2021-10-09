8769e0a9e3fad7c398fee611d336814442ea66ae
Dimitrije <test@gmail.com>
2021-10-09 08:48:58 +0200

    Wiederholung - ergänzung zu Gestern

--- src/ArithmetischeOperationen.java
+++ src/ArithmetischeOperationen.java
@@ -1,7 +1,23 @@
 public class ArithmetischeOperationen
 {
+    //psvm+TAB
+    //main+TAB
+    //public static => bitte immer so verwenden
+    //sout+TAB
+    //soutv
+
     public static void main(String[] args)
     {
+        //Ganzzahlen: byte, short, int, long
+        //Dezimalzahlen: (float), double
+        //Wahrheitstyp: boolean -> true, false
+        //Character: char
+        //Zeichenkette: String
+
+        double pi; //Deklaration
+        pi = 3.14; //Initialisierung
+
+
         int a = 5;
         int b = 10;
 
@@ -10,6 +26,35 @@ public class ArithmetischeOperationen
         System.out.println(b/a);
         System.out.println(a*b);
         System.out.println(a % b);
+
+        int c = 4;
+        int r = c++;
+        System.out.println(c);
+        System.out.println(r);
+
+        int z = 4;
+        int p = ++z;
+        System.out.println(z);
+        System.out.println(p);
+        printResult(200);
+        printResult(add(2, 5));
+
     }
+        //Methode -> Unterprogramm, Prozedur, Function, "Sub"
+        // public status -> ist fix, wenn wir direkt in der main-Anwendung programmiert
+        // Rückgabewert -> nochts -> void, spezieller Datentyp (int, double,...)
+        // namen -> naming convention -> camelCase
+        //Parameterliste -> Die Variablen, welche die Methode (ich) benötigt, um einen Auftrag zu erfüllen
+
+
+        public static void printResult(int result)
+        {
+            System.out.println(result);
+        }
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+
 }
 
--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,45 @@
 public class Calculator {
 
     public static void main(String[] args) {
-        short kleinsterDatentyp = 1;
-        int mittelDatentyp = 123;
-        long großerDatentyp = 13456987;
-        char a = 'a';
-        String Zeichenkette = "TEST";
-        System.out.println(kleinsterDatentyp + " " + mittelDatentyp + "  " + großerDatentyp + "  " + a + "  " + Zeichenkette);
+//        short kleinsterDatentyp = 1;
+//        int mittelDatentyp = 123;
+//        long großerDatentyp = 13456987;
+//        char a = 'a';
+//        String Zeichenkette = "TEST";
+//        System.out.println(kleinsterDatentyp + " " + mittelDatentyp + "  " + großerDatentyp + "  " + a + "  " + Zeichenkette);
+
+        double c = add(3,4);
+        System.out.println("Ergebnis add: " + c);
+        double c1 = sub(3,4);
+        System.out.println("Ergebnis sub: " + c1);
+        double c2 = mul(3,4);
+        System.out.println("Ergebnis mul: " + c2);
+        double c3 = div(3,4);
+        System.out.println("Ergebnis div: " + c3);
+        double c4 = mod(3,4);
+        System.out.println("Ergebnis mod: " + c4);
+
+
     }
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int mul(int a, int b)
+        {
+            return a * b;
+        }
+        public static int div(int a, int b)
+        {
+            return a / b;
+        }
+        public static int mod(int a, int b)
+        {
+            return a % b;
+        }
+
 }
